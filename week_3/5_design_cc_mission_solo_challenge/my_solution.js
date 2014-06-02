960894daafc07f4531daf909d9b34f1ecf59a116
Widny <widnystlouis1@gmail.com>
2014-06-02 01:27:44 -0400

    solo challenge added

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,12 +1,21 @@
 // This is a solo challenge
 
-// Your mission description:
+// Your mission description: Tarin becomes a wizard
 // 
 // 
 // 
 
 // Pseudocode
 // 
+//Tharin walks up to guarded door. 
+//Tharin attack guard1
+//Tharin attack guard 2
+//Tharin walks up to door
+//Tharin bust down door
+//Tharin walks up to golden cup
+//Tharin picks up cup
+//Tharin drink form cup
+//Tharin turns into wizard. 
 // 
 // 
 // 
@@ -27,7 +36,10 @@
 
 
 // Reflection
-// 
+// Still need to work on creating code, I am still having some trouble understanding javascript.
+// I will do the codecademy Javascript tack and try experimenting with it some more. 
+//It was fun coming up woth concept thougt. I would really love to be able to coeme back and write code
+//to seee Tharin become a wizard. 
 // 
 // 
 // 
